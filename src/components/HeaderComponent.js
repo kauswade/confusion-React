e5d34b84b5ca3bbb1bbd47b8f3b794262a318141
kaustubh wade <kaustubhwade97@gmail.com>
2020-09-10 21:50:59 +0530

    Single Page Applications Part 1

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -45,7 +45,7 @@ class Header extends Component {
                                 </NavItem>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/contactus">
-                                        <span className="fa fa-address-csrd fa-lg"></span> Contact Us
+                                        <span className="fa fa-address-card fa-lg"></span> Contact Us
                                 </NavLink>
                                 </NavItem>
                             </Nav>
